14740801a40feeb7dc86e7a00c03cc272740c126
Augusto Prodohl <51026238+xLonelyPlayer@users.noreply.github.com>
2024-05-08 22:19:22 -0300

    Adicionado sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'customodoro';
+  opened: boolean = true;
+
+  handleSidenavOpen(): void {
+    return;
+  }
+
+  handleSidenavClosed(): void {
+    return;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,29 @@ import { SettingsComponent } from './components/settings/settings.component';
 import { ModalComponent } from './components/modal/modal.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MenuComponent } from './components/menu/menu.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     SettingsComponent,
     ModalComponent,
     DashboardComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [
     provideAnimationsAsync()
